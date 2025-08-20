a6d86f017d520304ed38be13ee418c7d3a1f121c
Md. Naimur Rahman <84018165+EtherSphere01@users.noreply.github.com>
2025-08-20 11:48:42 +0600

    first anim

--- ultimate-gsap-starter/pages/02b_understanding-methods/script.js
+++ ultimate-gsap-starter/pages/02b_understanding-methods/script.js
@@ -1,1 +1,49 @@
 import gsap from "gsap";
+
+const play = document.querySelector(".play");
+const pause = document.querySelector(".pause");
+const resume = document.querySelector(".resume");
+const reverse = document.querySelector(".reverse");
+const restart = document.querySelector(".restart");
+const repeat = document.querySelector(".repeat");
+const kill = document.querySelector(".kill");
+const yoyo = document.querySelector(".yoyo");
+
+const animation = gsap.to(".box", {
+    opacity: 1,
+    rotation: 360,
+    borderRadius: "50%",
+    scale: 1.25,
+    duration: 2,
+});
+
+play.addEventListener("click", () => {
+    animation.play();
+});
+
+pause.addEventListener("click", () => {
+    animation.pause();
+});
+
+resume.addEventListener("click", () => {
+    animation.resume();
+});
+
+reverse.addEventListener("click", () => {
+    animation.reverse();
+});
+
+restart.addEventListener("click", () => {
+    animation.restart(true);
+});
+
+kill.addEventListener("click", () => {
+    animation.kill();
+});
+
+yoyo.addEventListener("click", () => {
+    animation.yoyo(true);
+});
+repeat.addEventListener("click", () => {
+    animation.repeat(2);
+});
